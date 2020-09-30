a0c59bf6beed7c59424eb5431a63ea031de61a9e
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-30 10:23:53 -0600

    Added some notes on Variables

--- hello-world.php
+++ hello-world.php
@@ -1,3 +1,21 @@
+This not not in PHP!!!
+
 <?php
-echo "Hello World...!"
+// 'echo' and 'print' are function we can use to output strings as responses...!
+/**
+ * in PHP, functions do not care about case sensitivity. echo = ECHO = eChO in terms of validity...!
+ * 
+ */
+echo "Hello World...in PHP!";
+
+/**
+ * Variable names start with a Dollar-sign ($)
+ * Note that unlike functions, they ARE case sensitive...!
+ * Also, don't start them with numbers or special characters...!
+ */
+
+$studentName = "\n\nFahad";
+
+echo $studentName;
 ?>
+
