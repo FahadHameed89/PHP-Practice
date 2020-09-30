cc7b94cdc1dee5a6bd65cf33ec9fed14f9a7fb9e
FahadHameed89 <69335527+FahadHameed89@users.noreply.github.com>
2020-09-30 13:24:29 -0600

    Added some PHP and replaced some strings in HTML with PHP echoes

--- index.php
+++ index.php
@@ -1,13 +1,18 @@
+<?php 
+    $pageTitle = 'Basic PHP Webpage bruh';
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index - Basic PHP Webpage</title>
+    <title><?php echo $pageTitle ?></title>
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <script type="text/JavaScript" src="js/scripts.js" defer></script>
 </head>
 <body>
-    <h1>Basic PHP Webpage</h1>
+    <h1><?php echo $pageTitle ?></h1>
 </body>
 </html>
